97bef8fec8c6e425081b64fa9ea70401644d64f2
Arman <145169221+ARMANPREET2003@users.noreply.github.com>
2024-06-08 17:17:41 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<!--/index.php -->
+<?php include 'includes/header.php';?>
+<main>
+    <h1> welcome to my website</h1>
+    <p>this is the home page</p>
+</main>
+
+<?php include'includes/footer.php';?>
